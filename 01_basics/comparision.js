96864d517d1e5a970b3ebf7d8b27873d0df5416f
ayusht28 <158249166+ayusht28@users.noreply.github.com>
2025-02-22 06:50:36 +0000

    compare

---
+++ 01_basics/comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//== equity check works diffrentlly in (<,>)compariosion converts null to a zero n then checks
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+// checks ==== if the datatype is same or not 
+//in this case datatype is not same one is string and the othe is number
